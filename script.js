b4a0aec2d6dbee110e78e3eb8cfc288d167abe6e
Your Name <you@example.com>
2019-11-12 17:09:05 +0400

    12 noemberi

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let admin, name;
+var feri = document.querySelector(".qv");
+feri.addEventListener('mousemove', feris);
 
-name = "mogesalmebit";
-admin = name;
+function feris (e){
+	//feri.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40)"
+	document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",30)"
+}
 
+window.addEventListener('load', ()=>{
+    const animacia = document.querySelector('animacia');
+    animacia.classList.add('animacia-end');
+})
